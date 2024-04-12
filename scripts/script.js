fa5ff1c58977eb6c893309bfa148eddf48bff78b
Shada <43104909+shada12@users.noreply.github.com>
2024-04-12 07:47:11 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ function triangleArea() {
     if (side1 <= 0 || side2 <= 0 || side3 <= 0 ||
         (side1 + side2 <= side3) || (side1 + side3 <= side2) ||
         (side2 + side3 <= side1)) {
-        document.getElementById("displayArea").innerHTML = "This is not valid triangle";
+         document.getElementById("displayArea").innerHTML = "This is not valid triangle";
     }
     else {
 
